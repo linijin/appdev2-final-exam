19bb805cf4a68835bad35a7f74ca4161b664c5d2
linijin <wrthlssjane@gmail.com>
2025-06-07 13:42:17 +0800

    Task 2: Add user authentication

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  res.json({ token });
+};
